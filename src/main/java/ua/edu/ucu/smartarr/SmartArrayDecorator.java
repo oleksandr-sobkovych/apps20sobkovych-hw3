efa38c54df946dfabecd02e2427f08242476b369
Alex-quickcoder <oleksandr.sobkovych@gmail.com>
2020-11-27 15:44:47 +0200

    fixed warnings

--- src/main/java/ua/edu/ucu/smartarr/SmartArrayDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/SmartArrayDecorator.java
@@ -5,16 +5,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 abstract class SmartArrayDecorator<T> implements SmartArray<T> {
-    @Setter(AccessLevel.PROTECTED)
+    @Setter(AccessLevel.PROTECTED) @Getter(AccessLevel.PROTECTED)
     private Object[] array;
 
     @Override
     public Object[] toArray() {
-        return this.array.clone();
+        return getArray().clone();
     }
 
     @Override
     public int size() {
-        return this.array.length;
+        return getArray().length;
     }
 }
